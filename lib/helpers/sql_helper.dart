38a6161ae6113dde45040de8d3fbdb09fa26fbd6
Siwar Soula <siwarsoula@gmail.com>
2024-03-06 18:11:07 +0100

    CRUD works nicely on emulator

--- lib/helpers/sql_helper.dart
+++ lib/helpers/sql_helper.dart
@@ -2,50 +2,55 @@ import 'package:flutter/foundation.dart';
 import 'package:sqflite/sqflite.dart' as sql;
 
 class SQLHelper {
-
-////Data Table
   static Future<void> createTables(sql.Database database) async {
     await database.execute("""CREATE TABLE items(
-      id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-      title TEXT,
-      description TEXT,
-      createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-    )
-    """);
+        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+        title TEXT,
+        description TEXT,
+        createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+      )
+      """);
   }
-////Create a DB
+// id: the id of a item
+// title, description: name and description of your activity
+// created_at: the time that the item was created. It will be automatically handled by SQLite
+
   static Future<sql.Database> db() async {
     return sql.openDatabase(
-      'dbestech.db',
+      'dbtech.db',
       version: 1,
       onCreate: (sql.Database database, int version) async {
         await createTables(database);
       },
     );
   }
-////Data Insert
+
+  // Create new item (journal)
   static Future<int> createItem(String title, String? descrption) async {
     final db = await SQLHelper.db();
 
     final data = {'title': title, 'description': descrption};
-    final id = await db.insert(
-      'items', //table name to insert into
-      data,
-      conflictAlgorithm: sql.ConflictAlgorithm.replace);
+    final id = await db.insert('items', data,
+        conflictAlgorithm: sql.ConflictAlgorithm.replace);
     return id;
   }
-//// Get All Data
+
+  // Read all items (journals)
   static Future<List<Map<String, dynamic>>> getItems() async {
     final db = await SQLHelper.db();
     return db.query('items', orderBy: "id");
   }
-//// Get Specific Data
+
+  // Read a single item by id
+  // The app doesn't use this method but I put here in case you want to see it
   static Future<List<Map<String, dynamic>>> getItem(int id) async {
-    final db = await SQLHelper.db(); //make db connection
+    final db = await SQLHelper.db();
     return db.query('items', where: "id = ?", whereArgs: [id], limit: 1);
   }
-//// Update an item by id
-  static Future<int> updateItem(int id, String title, String? description) async {
+
+  // Update an item by id
+  static Future<int> updateItem(
+      int id, String title, String? description) async {
     final db = await SQLHelper.db();
 
     final data = {
@@ -58,7 +63,8 @@ class SQLHelper {
     await db.update('items', data, where: "id = ?", whereArgs: [id]);
     return result;
   }
-//// Delete item by id
+
+  // Delete
   static Future<void> deleteItem(int id) async {
     final db = await SQLHelper.db();
     try {
--- lib/main.dart
+++ lib/main.dart
@@ -6,30 +6,29 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(),
-    );
+      // Remove the debug banner
+        debugShowCheckedModeBanner: false,
+        title: 'SQLITE',
+        theme: ThemeData(
+          primarySwatch: Colors.orange,
+        ),
+        home: const HomePage());
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _HomePageState extends State<HomePage> {
   // All journals
   List<Map<String, dynamic>> _journals = [];
 
@@ -46,36 +45,142 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     super.initState();
+    _refreshJournals(); // Loading the diary when the app starts
+  }
+
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+
+  // This function will be triggered when the floating button is pressed
+  // It will also be triggered when you want to update an item
+  void _showForm(int? id) async {
+    if (id != null) {
+      // id == null -> create new item
+      // id != null -> update an existing item
+      final existingJournal =
+      _journals.firstWhere((element) => element['id'] == id);
+      _titleController.text = existingJournal['title'];
+      _descriptionController.text = existingJournal['description'];
+    }
+
+    showModalBottomSheet(
+        context: context,
+        elevation: 5,
+        isScrollControlled: true,
+        builder: (_) => Container(
+          padding: EdgeInsets.only(
+            top: 15,
+            left: 15,
+            right: 15,
+            // this will prevent the soft keyboard from covering the text fields
+            bottom: MediaQuery.of(context).viewInsets.bottom + 120,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              TextField(
+                controller: _titleController,
+                decoration: const InputDecoration(hintText: 'Title'),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              TextField(
+                controller: _descriptionController,
+                decoration: const InputDecoration(hintText: 'Description'),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  // Save new journal
+                  if (id == null) {
+                    await _addItem();
+                  }
+
+                  if (id != null) {
+                    await _updateItem(id);
+                  }
+
+                  // Clear the text fields
+                  _titleController.text = '';
+                  _descriptionController.text = '';
+
+                  // Close the bottom sheet
+                  Navigator.of(context).pop();
+                },
+                child: Text(id == null ? 'Create New' : 'Update'),
+              )
+            ],
+          ),
+        ));
+  }
+
+// Insert a new journal to the database
+  Future<void> _addItem() async {
+    await SQLHelper.createItem(
+        _titleController.text, _descriptionController.text);
     _refreshJournals();
-    print("number of items ${_journals.length}"); // Loading the diary when the app starts
+  }
+
+  // Update an existing journal
+  Future<void> _updateItem(int id) async {
+    await SQLHelper.updateItem(
+        id, _titleController.text, _descriptionController.text);
+    _refreshJournals();
+  }
+
+  // Delete an item
+  void _deleteItem(int id) async {
+    await SQLHelper.deleteItem(id);
+    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+      content: Text('Successfully deleted a journal!'),
+    ));
+    _refreshJournals();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('Sqlite with Flutter App'),
+        title: const Text('SQL'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              'counter was here',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: _isLoading
+          ? const Center(
+        child: CircularProgressIndicator(),
+      )
+          : ListView.builder(
+        itemCount: _journals.length,
+        itemBuilder: (context, index) => Card(
+          color: Colors.orange[200],
+          margin: const EdgeInsets.all(15),
+          child: ListTile(
+              title: Text(_journals[index]['title']),
+              subtitle: Text(_journals[index]['description']),
+              trailing: SizedBox(
+                width: 100,
+                child: Row(
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.edit),
+                      onPressed: () => _showForm(_journals[index]['id']),
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.delete),
+                      onPressed: () =>
+                          _deleteItem(_journals[index]['id']),
+                    ),
+                  ],
+                ),
+              )),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
-        tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: () => _showForm(null),
+      ),
     );
   }
 }
